ba44ad69d79f04b046e56a6790bfc326c7b200a4
Chintan Akbari <cna@taglineinfotech.com>
2025-06-11 05:45:47 +0000

    add

--- next.config.ts
+++ next.config.ts
@@ -27,6 +27,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "static.seekingalpha.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
   allowedDevOrigins: [
