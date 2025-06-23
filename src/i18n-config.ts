d98105667917f37781d5cc8603c64afec8858a92
Chintan Akbari <cna@taglineinfotech.com>
2025-06-23 09:20:38 +0000

    Please remove [locale] folder

--- src/i18n-config.ts
+++
@@ -1,7 +0,0 @@
-
-export const locales = ["en", "fr", "es"];
-export const defaultLocale = "en";
-
-export function isValidLocale(locale: string): boolean {
-  return (locales as readonly string[]).includes(locale);
-}
